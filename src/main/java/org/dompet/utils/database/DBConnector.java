8f95f2ffca4fd2e1c2ee919157d2e20d2d10bcae
MiiJack <f.rama723@gmail.com>
2024-03-24 17:27:50 +0300

    chore: formatting

--- src/main/java/org/dompet/utils/database/DBConnector.java
+++ src/main/java/org/dompet/utils/database/DBConnector.java
@@ -1,26 +1,22 @@
 package org.dompet.utils.database;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DBConnector {
-    private final DBProperties dbProperties;
+  private final DBProperties dbProperties;
 
-    public DBConnector(DBProperties dbProperties) {
-        this.dbProperties = dbProperties;
-    }
+  public DBConnector(DBProperties dbProperties) {
+    this.dbProperties = dbProperties;
+  }
 
-    @Bean
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-            dbProperties.getUrl(),
-            dbProperties.getUser(),
-            dbProperties.getPassword()
-        );
-    }
+  @Bean
+  public Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(
+        dbProperties.getUrl(), dbProperties.getUser(), dbProperties.getPassword());
+  }
 }
