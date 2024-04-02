cfdf19b120af3586eb643bd15e3913921cdb0561
MiiJack <f.rama723@gmail.com>
2024-04-02 11:10:00 +0300

    feat: implemented Client endpoint

--- src/main/java/org/dompet/controller/ClientController.java
+++ src/main/java/org/dompet/controller/ClientController.java
@@ -3,34 +3,39 @@ package org.dompet.controller;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.dompet.model.Account;
 import org.dompet.model.Client;
 import org.dompet.service.ClientService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin
 @RequestMapping("/client")
 public class ClientController {
   private final ClientService clientService;
 
   @GetMapping
   public List<Client> findAllClients() {
-    return clientService.findAll();
+    return clientService.findAllClients();
+  }
+
+  @GetMapping("/{clientId}/accounts")
+  public List<Account> findAllAccounts(@PathVariable String clientId) {
+    return clientService.findAllAccounts(clientId);
   }
 
   @GetMapping("/{id}")
   public Optional<Client> findClientById(@PathVariable String id) {
-    return clientService.findById(id);
+    return clientService.findClientById(id);
   }
 
   @PutMapping
   public Client saveClient(@RequestBody Client client) {
-    return clientService.save(client);
+    return clientService.saveClient(client);
   }
 
   @DeleteMapping("/{id}")
   public void deleteClientById(@PathVariable String id) {
-    clientService.deleteById(id);
+    clientService.deleteClientById(id);
   }
 }
--- src/main/java/org/dompet/service/ClientService.java
+++ src/main/java/org/dompet/service/ClientService.java
@@ -2,31 +2,48 @@ package org.dompet.service;
 
 import java.util.List;
 import java.util.Optional;
+import org.dompet.model.Account;
 import org.dompet.model.Client;
+import org.dompet.repository.AccountRepository;
 import org.dompet.repository.ClientRepository;
+import org.dompet.utils.EntityUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
   public final ClientRepository clientRepository;
+  public final AccountRepository accountRepository;
 
-  public ClientService(ClientRepository clientRepository) {
+  public ClientService(ClientRepository clientRepository, AccountRepository accountRepository) {
     this.clientRepository = clientRepository;
+    this.accountRepository = accountRepository;
   }
 
-  public Client save(Client client) {
-    return clientRepository.save(client);
+  public Client saveClient(Client client) {
+    if (clientRepository.findById(client.getClientId()).isEmpty()) {
+      return clientRepository.save(client);
+    }
+    Client existingClient =
+        clientRepository
+            .findById(client.getClientId())
+            .orElseThrow(() -> new RuntimeException("Client not found"));
+    EntityUtil.updateEntityFields(existingClient, client);
+    return clientRepository.save(existingClient);
   }
 
-  public Optional<Client> findById(String id) {
+  public List<Account> findAllAccounts(String ClientId) {
+    return accountRepository.findByClientId(ClientId);
+  }
+
+  public Optional<Client> findClientById(String id) {
     return clientRepository.findById(id);
   }
 
-  public List<Client> findAll() {
+  public List<Client> findAllClients() {
     return clientRepository.findAll();
   }
 
-  public void deleteById(String id) {
+  public void deleteClientById(String id) {
     clientRepository.deleteById(id);
   }
 }
