95eac6d2d90397cedde9feb15d043b1ebc7badeb
MiiJack <f.rama723@gmail.com>
2024-04-02 11:08:22 +0300

    chore: removed unused repository

--- src/main/java/org/dompet/repository/AccountBalanceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dompet.repository;
-
-import org.dompet.model.AccountBalance;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountBalanceRepository {
-  AccountBalance findByAccountId(String accountId);
-}
