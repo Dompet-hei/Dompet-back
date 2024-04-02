ceb4500c3745d1a842b5b063b90084a0760907d7
tendryAxel <hei.tendry@gmail.com>
2024-04-02 22:25:04 +0300

    feat: function to find id column name

---
+++ src/main/java/org/dompet/utils/annotations/IdAnnotation.java
@@ -0,0 +1,17 @@
+package org.dompet.utils.annotations;
+
+import org.dompet.model.Account;
+
+import java.util.Arrays;
+
+public class IdAnnotation {
+    static String getIdColumnName(Class<?> clazz){
+        return Arrays
+                .stream(clazz.getDeclaredFields())
+                .filter(champ -> champ.isAnnotationPresent(Id.class))
+                .toList()
+                .get(0)
+                .getAnnotation(Column.class)
+                .name();
+    }
+}
